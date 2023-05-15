1f9f2d4cc2d29fcd45918c59207041beb5c5bf82
Kévin <kevindubuc62@gmail.com>
2023-05-15 15:27:08 +0200

    init request method

--- lib/metarisc.d.ts
+++ lib/metarisc.d.ts
@@ -1,2 +1,18 @@
-export declare class Metarisc {
+import { AxiosResponse } from "axios";
+interface RequestConfig {
+    body?: any;
+    headers?: string | {
+        [name: string]: string | string[];
+    };
+    params?: {
+        [param: string]: string | string[];
+    };
+    endpoint?: string;
+    method?: string;
 }
+export declare class Metarisc {
+    private axios;
+    constructor();
+    request<T>(config: RequestConfig): Promise<AxiosResponse<T>>;
+}
+export {};
--- src/metarisc.ts
+++ src/metarisc.ts
@@ -1,3 +1,31 @@
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+
+interface RequestConfig {
+    body ?: any,
+    headers ?: string | {[name: string]: string | string[]},
+    params ?: {[param: string]: string | string[]},
+    endpoint ?: string,
+    method ?: string
+};
+
 export class Metarisc
 {
+    private axios : AxiosInstance;
+
+    constructor()
+    {
+        this.axios = axios.create({
+            baseURL: 'https://api.metarisc.fr/'
+        });
+    }
+
+    async request<T>(config : RequestConfig) : Promise<AxiosResponse<T>>
+    {
+        return this.axios.request<T>({
+            method: config.method || 'GET',
+            url: config.endpoint || '/',
+            params: config.params,
+            data: config.body
+        });
+    }
 }
