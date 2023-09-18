6cf2731aecc725b8d9fcd6a02e39030eabb71bd4
lucasbvn <127110180+lucasbvn@users.noreply.github.com>
2023-09-18 09:58:32 +0200

    add test file

---
+++ tests/index.mjs
@@ -0,0 +1,9 @@
+import { Metarisc } from "../lib/index.js";
+
+const metarisc = new Metarisc({
+  metarisc_url: "https://testlp7ijwog-mock.functions.fnc.fr-par.scw.cloud/",
+});
+
+console.log(await metarisc.poi.getPoi("123456"));
+
+// npm run compile && node tests/index.mjs
