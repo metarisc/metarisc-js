ab7e3f993ab269ec71f7d6f7e6a764fe29c0eddd
lucasbvn <127110180+lucasbvn@users.noreply.github.com>
2024-02-05 17:15:39 +0100

    Delete GetFeedMessages2XXResponse.ts

--- src/model/GetFeedMessages2XXResponse.ts
+++
@@ -1,7 +0,0 @@
-import { FeedMessage } from '../../src/model/FeedMessage';
-import { PaginatePei200ResponseMeta } from '../../src/model/PaginatePei200ResponseMeta';
-
-export type GetFeedMessages2XXResponse = {
-    'data'?: Array<FeedMessage>;
-    'meta'?: PaginatePei200ResponseMeta;
-};
