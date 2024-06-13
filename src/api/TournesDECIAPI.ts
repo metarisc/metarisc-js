585f6c11876481c7318f4732bfb9d3786488d706
Kévin <kevindubuc62@gmail.com>
2024-06-13 11:20:31 +0200

    remove buggy getTourneeDeciLivretDeTournee method

--- src/api/TournesDECIAPI.ts
+++ src/api/TournesDECIAPI.ts
@@ -26,20 +26,6 @@ export class TournesDECIAPI extends Core {
     }
     
     /**
-     * Génération d'un livret de tournée pour une tournée DECI.
-     */
-    async getTourneeDeciLivretDeTournee(tourneeDeciId: string ): Promise<AxiosResponse<HttpFile>>
-    {
-        const pathVariable = { 'tournee_deci_id': (new String(tourneeDeciId)).toString() };
-        return this.request({
-            method: 'GET',
-            endpoint: Utils.constructPath(pathVariable, '/tournees_deci/{tournee_deci_id}/livret_de_tournee'),
-            params: {  },
-            body: Utils.payloadFilter({})
-        });
-    }
-    
-    /**
      * Récupération des détails liés au contrôle d'un PEI d'une tournée.
      */
     async getTourneeDeciPei(tourneeDeciId: string, peiId: string ): Promise<AxiosResponse<TourneeDeciPei>>
