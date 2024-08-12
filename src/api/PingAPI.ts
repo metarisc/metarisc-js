6c7ebadfe41e8f9afdbd7ed0d062f45856b53109
Kévin <kevindubuc62@gmail.com>
2024-08-12 11:06:25 +0200

    eslint rules applied

--- src/api/PingAPI.ts
+++ src/api/PingAPI.ts
@@ -1,6 +1,4 @@
 import { Core, MetariscConfig } from "../core";
-import { Utils } from "../utils";
-import type { AxiosResponse } from "axios";
 import { Client } from "../client";
 
 export class PingAPI extends Core {
