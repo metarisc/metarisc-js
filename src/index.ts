98e838edc88525f5fe0181c9d32d02f5580bb02d
Kévin <kevindubuc62@gmail.com>
2025-03-11 14:54:38 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,47 +6,4 @@ export { Collection, PaginationData, PaginationResults } from "./collection";
 export { AuthMethod, Client } from "./client";
 export { Utils } from "./utils";
 export { Tus } from "./tus";
-
-export { AnomaliesAPI } from './api/AnomaliesAPI';
-
-export { CommissionsAPI } from './api/CommissionsAPI';
-
-export { CommissionsMembresAPI } from './api/CommissionsMembresAPI';
-
-export { ContactsAPI } from './api/ContactsAPI';
-
-export { ControlesPEIAPI } from './api/ControlesPEIAPI';
-
-export { DatesPassageCommissionAPI } from './api/DatesPassageCommissionAPI';
-
-export { DocumentsAPI } from './api/DocumentsAPI';
-
-export { DossiersAPI } from './api/DossiersAPI';
-
-export { ERPAPI } from './api/ERPAPI';
-
-export { EvenementsAPI } from './api/EvenementsAPI';
-
-export { FeedAPI } from './api/FeedAPI';
-
-export { HealthcheckAPI } from './api/HealthcheckAPI';
-
-export { MoiAPI } from './api/MoiAPI';
-
-export { NotificationsAPI } from './api/NotificationsAPI';
-
-export { OrdresDuJourAPI } from './api/OrdresDuJourAPI';
-
-export { OrganisationsAPI } from './api/OrganisationsAPI';
-
-export { PEIAPI } from './api/PEIAPI';
-
-export { PingAPI } from './api/PingAPI';
-
-export { PrescriptionsAPI } from './api/PrescriptionsAPI';
-
-export { TournesDECIAPI } from './api/TournesDECIAPI';
-
-export { UtilisateursAPI } from './api/UtilisateursAPI';
-
-export { WorkflowsAPI } from './api/WorkflowsAPI';
+export * from './resources';
