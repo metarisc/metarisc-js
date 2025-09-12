09ce93fe9379afe414ee429c331505f6c6910f72
metarisc-openapi <171580474+metarisc-openapi[bot]@users.noreply.github.com>
2025-09-12 15:53:31 +0000

    Update SDK

--- src/model/FeedMessage.ts
+++
@@ -1,9 +0,0 @@
-// File generated from our OpenAPI spec
-import { FeedMessageTexte } from './FeedMessageTexte';
-
-export type FeedMessage = FeedMessageTexte;
-
-export enum TypeEnum {
-    TEXTE = 'texte',
-    LISTE_PEI = 'liste_pei'
-}
