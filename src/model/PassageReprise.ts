926981d624b126dae6c7884432beb9c67afc4518
metarisc-openapi <171580474+metarisc-openapi[bot]@users.noreply.github.com>
2026-07-28 12:50:44 +0000

    Update SDK

---
+++ src/model/PassageReprise.ts
@@ -0,0 +1,23 @@
+// File generated from our OpenAPI spec
+
+export type PassageReprise = {
+    'type_passage_commission': TypePassageCommissionEnum;
+};
+
+export enum TypePassageCommissionEnum {
+    SALLE = 'salle',
+    VISITE_RECEPTION_TRAVAUX_EN_GROUPE_DE_VISITE = 'visite_reception_travaux_en_groupe_de_visite',
+    VISITE_RECEPTION_AVANT_OUVERTURE_EN_GROUPE_DE_VISITE = 'visite_reception_avant_ouverture_en_groupe_de_visite',
+    VISITE_PERIODIQUE_EN_GROUPE_DE_VISITE = 'visite_periodique_en_groupe_de_visite',
+    VISITE_INOPINEE_EN_GROUPE_DE_VISITE = 'visite_inopinee_en_groupe_de_visite',
+    VISITE_CONTROLE_EN_GROUPE_DE_VISITE = 'visite_controle_en_groupe_de_visite',
+    VISITE_CHANTIER_EN_GROUPE_DE_VISITE = 'visite_chantier_en_groupe_de_visite',
+    VISITE_CONSEIL_EN_GROUPE_DE_VISITE = 'visite_conseil_en_groupe_de_visite',
+    VISITE_RECEPTION_TRAVAUX_EN_PLENIERE = 'visite_reception_travaux_en_pleniere',
+    VISITE_RECEPTION_AVANT_OUVERTURE_EN_PLENIERE = 'visite_reception_avant_ouverture_en_pleniere',
+    VISITE_PERIODIQUE_EN_PLENIERE = 'visite_periodique_en_pleniere',
+    VISITE_INOPINEE_EN_PLENIERE = 'visite_inopinee_en_pleniere',
+    VISITE_CONTROLE_EN_PLENIERE = 'visite_controle_en_pleniere',
+    VISITE_CHANTIER_EN_PLENIERE = 'visite_chantier_en_pleniere',
+    VISITE_CONSEIL_EN_PLENIERE = 'visite_conseil_en_pleniere'
+}
